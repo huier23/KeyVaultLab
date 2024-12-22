a27e7e129c8224d7030a126772a896038436af75
Huier Huang <huihuan@microsoft.com>
2024-12-22 18:19:17 +0800

    update for lab env

--- KeyVaultLab/Controllers/HomeController.cs
+++ KeyVaultLab/Controllers/HomeController.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
+using System.IO;
 using System.Linq;
+using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
 
@@ -8,21 +11,70 @@ namespace KeyVaultLab.Controllers
 {
     public class HomeController : Controller
     {
-        public ActionResult Index()
+        // Lab: get values from Web.config and Key Vault
+        public async Task<ActionResult> Index()
         {
+            // From Web.config
+            string db = ConfigurationManager.ConnectionStrings["ConnDb"].ConnectionString;
+            string sec = ConfigurationManager.AppSettings["SecretFromKeyVault"];
+            string env = ConfigurationManager.AppSettings["Environment"];
+            ViewBag.WebConfigEnv = $"From {env}";
+            ViewBag.WebConfigDb = $"DB: {db}";
+            ViewBag.WebConfigSecret = $"Secret: {sec}";
+
+            // TODO
             return View();
         }
 
-        public ActionResult About()
+        // Lab: get values from Key Vault with client service
+        public async Task<ActionResult> About()
         {
-            ViewBag.Message = "Your application description page.";
+            try
+            {
+                string secretValue = ConfigurationManager.AppSettings["SecretFromKeyVault"];
+                // TODO
+                ViewBag.Secret = $"Secret from: {secretValue}";
+            }
+            catch (Exception exp)
+            {
+                ViewBag.Error = $"Something went wrong: {exp.Message}";
+            }
 
             return View();
         }
 
-        public ActionResult Contact()
+        // Lab: get data from Azure Blob Storage
+        public async Task<ActionResult> Contact()
         {
-            ViewBag.Message = "Your contact page.";
+            // Azure Blob Storage Infomation
+            string containerName = "keyvaultdemo";
+            string blobName = "employee.json";
+
+            // Connect Azure Blob with Managed Identity
+            try
+            {
+                // TODO
+            }
+            catch (Exception exp)
+            {
+                Console.WriteLine(exp.Message, exp.StackTrace);
+                ViewBag.Error = $"Something went wrong: {exp.Message}";
+            }
+
+
+            // Connect Azure Blob with Connection String
+            try
+            {
+                // Azure Blob Storage code
+                string blobConnectionString = ConfigurationManager.AppSettings["ConnSg"];
+
+                // TODO
+            }
+            catch (Exception exp)
+            {
+                Console.WriteLine(exp.Message);
+                ViewBag.Error = $"Something went wrong: {exp.Message}";
+            }
 
             return View();
         }
